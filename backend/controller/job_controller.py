7bf92dbc659f0f1a0e3734ca10f0fa334cc138ba
vishnuhari17 <vishnuhari.anup@gmail.com>
2025-03-22 11:42:16 +0530

    added token in header

--- backend/controller/job_controller.py
+++ backend/controller/job_controller.py
@@ -1,18 +1,22 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from models.job_model import Jobs
+from fastapi import Header, Depends
 
 router = APIRouter(
     prefix="/job",
     tags=["job"],
 )
 
+def get_token(authorization: str = Header(...)):
+    return authorization.split(" ")[1]
+
 # Define request body model
 class JobSearchRequest(BaseModel):
     query: str
 
 @router.post("/search")
-async def search(request: JobSearchRequest):
+async def search(request: JobSearchRequest,token: str = Depends(get_token)):
     job = Jobs()
     return job.search_jobs_vector(request.query)
 
