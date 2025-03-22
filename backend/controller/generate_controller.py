689c8cf9b3ab4959f7ec1f5c396d3ae35455fb8c
vishnuhari17 <vishnuhari.anup@gmail.com>
2025-03-22 22:54:18 +0530

    added resume code

--- backend/controller/generate_controller.py
+++ backend/controller/generate_controller.py
@@ -4,6 +4,8 @@ import os
 from models.user_model import Users
 from models.userdetails_model import UserDetails
 from models.generate_model import GenerateResume
+from pydantic import BaseModel
+
 
 
 router = APIRouter(
@@ -11,22 +13,31 @@ router = APIRouter(
     tags=["generate"],
 )
 
+class GenerateResumeRequest(BaseModel):
+    job_desc: str
+
 def get_token(authorization: str = Header(...)):
     return authorization.split(" ")[1]
 
 @router.post("/")
-async def get_user_details(job_desc,token: str = Depends(get_token)):
-    print(token)
+async def generate_resume(input: GenerateResumeRequest, token: str = Depends(get_token)):
     try:
         users = Users()
         payload = users.verify_jwt(token)
+        
+        if not payload or "username" not in payload:
+            return {"error": "Invalid or expired token"}
+        
         username = payload["username"]
         user_details = UserDetails().get_user_details(username)
         
         if isinstance(user_details, dict) and '_id' in user_details:
             user_details['_id'] = str(user_details['_id'])
+        
+        print(user_details)
+        print(input.job_desc)
 
-        resume = GenerateResume().get_user_details(user_details, job_desc)
+        GenerateResume().get_user_details(user_details, input.job_desc)
         # Save the resume as a PDF file in the output folder
         output_folder = "output"
         os.makedirs(output_folder, exist_ok=True)
